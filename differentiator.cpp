3b1f7ee8fea5bdd7a9df11bb2d0d52dbfbd33227
ilyaytrewq <tixonov.ilay.2000@mail.ru>
2025-07-25 23:48:02 +0300

    refactor code

--- differentiator.cpp
+++ differentiator.cpp
@@ -12,54 +12,28 @@ int main(int argc, char *argv[])
     }
     std::string type = argv[1];
 
-    bool is_comp = 0;
-    for (int i = 2; i < argc; ++i)
-    {
-        is_comp |= is_complex(argv[i]);
-    }
-
     if (type == "--eval")
     {
         std::string expr_str = argv[2];
-        if (!is_comp)
+        std::map<std::string, Real> vars;
+        for (int i = 3; i < argc; ++i)
         {
-            std::map<std::string, Real> vars;
-            for (int i = 3; i < argc; ++i)
-            {
-                std::string Var = argv[i];
-                int pos = Var.find("=");
-                std::string var = Var.substr(0, pos);
-                Real val = stold(Var.substr(pos + 1));
-                if (vars.count(var))
-                    throw std::runtime_error("2 times one variable");
-                vars[var] = val;
-            }
-            std::cout << make_expression<Real>(expr_str).eval(vars) << '\n';
+            std::string Var = argv[i];
+            int pos = Var.find("=");
+            std::string var = Var.substr(0, pos);
+            Real val = stold(Var.substr(pos + 1));
+            if (vars.count(var))
+                throw std::runtime_error("2 times one variable");
+            vars[var] = val;
         }
-        else{
-            std::map<std::string, Complex> vars;
-            for (int i = 3; i < argc; ++i)
-            {
-                std::string Var = argv[i];
-                int pos = Var.find("=");
-                std::string var = Var.substr(0, pos);
-                Complex val = ParseComplex(Var.substr(pos + 1));
-                if (vars.count(var))
-                    throw std::runtime_error("2 times one variable");
-                vars[var] = val;
-            }
-            std::cout << make_expression<Complex>(expr_str).eval(vars) << '\n';
-        }
+        std::cout << make_expression<Real>(expr_str).eval(vars) << '\n';
     }
     else if (type == "--diff")
     {
         if (argc != 5 || std::string(argv[3]) != "--by")
             throw std::runtime_error("Invalid request");
         std::string expr_str = argv[2];
-        if(!is_comp)
-            std::cout << make_expression<Real>(expr_str).diff(argv[4]) << '\n';
-        else
-        std::cout << make_expression<Complex>(expr_str).diff(argv[4]) << '\n';
+        std::cout << make_expression<Real>(expr_str).diff(argv[4]) << '\n';
     }
     else
     {
